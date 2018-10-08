b182438c771e25db4812412b25ba254a1664a1e4
OlehZ <oleh.zaslavskyy@gmail.com>
2018-10-08 12:09:09 +0300

    some animation add

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(window).scroll(function() {
+    $('.mov1').each(function(){
+      var imagePos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+550) {
+        $(this).addClass('pulse');
+      }
+    });
+  });
+  $(window).scroll(function() {
+    $('.mov2').each(function(){
+      var imagePos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+200) {
+        $(this).addClass('bounceInRight');
+      }
+    });
+  });
+  $(window).scroll(function() {
+    $('.mov3').each(function(){
+      var imagePos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+400) {
+        $(this).addClass('bounceIn');
+      }
+    });
+  });
