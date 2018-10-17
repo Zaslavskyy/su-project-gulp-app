7e0ef71ff235972f6a87e1816578c9206c7c4784
OlehZ <oleh.zaslavskyy@gmail.com>
2018-10-17 14:04:18 +0300

    ajax change

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+/* 
 function loadParagraph() {
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
@@ -8,9 +8,19 @@ xhr.onreadystatechange = function() {
 };
 xhr.open('GET', 'data/data.html', true);
 xhr.send('null');
+} */
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("about").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "data/data.html", true);
+  xhttp.send();
 }
-
-
 
 
 $(window).scroll(function() {
