565040c141800db6b9b8c48ef89f07ad4dd0977f
OlehZ <oleh.zaslavskyy@gmail.com>
2018-10-19 11:13:02 +0300

    modal add & ajax fix

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
-
-
-function loadParagraph() {
+var load = document.getElementById("loadText");
+load.onclick = function() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -12,6 +11,20 @@ function loadParagraph() {
   xhttp.send();
 }
 
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("modalBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span. onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 $(window).scroll(function() {
     $('.mov1').each(function(){
